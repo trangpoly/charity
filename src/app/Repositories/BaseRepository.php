b143205055d076d033f06d61e3e8a0b9f0d2d032
Nguyen Truong Son <son.nguyen@tda.company>
2022-09-28 11:05:47 +0700

    Paginate data

--- src/app/Repositories/BaseRepository.php
+++ src/app/Repositories/BaseRepository.php
@@ -2,16 +2,62 @@
 
 namespace App\Repositories;
 
-class BaseRepository implements BaseRepositoryInterface
+abstract class BaseRepository implements BaseRepositoryInterface
 {
+    const PAGE_LIMIT = 10;
+
     /**
      * @var \App\Models\BaseModel | \Illuminate\Database\Eloquent\Builder
      */
     public $model;
 
-    public function __construct(\App\Models\BaseModel $model)
+    public function __construct()
     {
-        $this->model = $model;
+        $this->model = $this->makeModel();
+    }
+
+    abstract public function model();
+
+    /**
+     * @return \App\Models\BaseModel | \Illuminate\Database\Eloquent\Builder
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    private function makeModel()
+    {
+        return app()->make($this->model());
+    }
+
+    public function all()
+    {
+        return $this->model->all();
+    }
+
+    public function paginate(array $options = [], $limit = self::PAGE_LIMIT)
+    {
+        $filter = collect($options);
+
+        $query = $this->model;
+
+        if ($filter->has('with')) {
+            $query = $query->with($filter->get('with'));
+        }
+
+        if ($filter->has('sort')) {
+            $sort = $filter->get('sort');
+            if (!is_array($sort)) {
+                $field    = $sort;
+                $sortType = 'ASC';
+
+                if (substr($sort, 0, 1) === '-') {
+                    $field    = substr($sort, 1, strlen($sort) - 2);
+                    $sortType = 'DESC';
+                }
+
+                $query = $query->orderBy($field, $sortType);
+            }
+        }
+
+        return $query->paginate($limit);
     }
 
     public function find($id)
