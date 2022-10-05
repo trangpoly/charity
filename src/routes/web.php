56eeaae96090fef665a91536801bce35049776c9
trang.pham <fresher.trang.pham@mohasoftware.com>
2022-10-05 16:11:27 +0700

    commit layout header-footer-mypage

--- src/routes/web.php
+++ src/routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/my-app', function () {
+    return view('pages.my-page.subscribe-receive');
+})->middleware(['auth'])->name('my-page.subscribe-receive');
+
 require __DIR__.'/auth.php';
