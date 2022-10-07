787166e5325f338b7551ad6d5c1f9b4ec27a79af
pho.lai <frescher.pho.lai@mohasoftware.com>
2022-10-07 14:58:20 +0700

    feat: 🎸 Products

--- src/app/Models/Product.php
+++ src/app/Models/Product.php
@@ -10,6 +10,25 @@ class Product extends BaseModel
     use HasFactory;
     use Uuid;
 
+    protected $table = 'products';
+
+    protected $fillable = [
+        'name',
+        'desc',
+        'avatar',
+        'unit',
+        'weight',
+        'expiration',
+        'quantity',
+        'weight_unit',
+        'address',
+        'phone',
+        'stock',
+        'status',
+        'category_id',
+        'owner_id',
+    ];
+
     public function images()
     {
         return $this->hasMany(ProductImage::class, 'product_id', 'id');
