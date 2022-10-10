b552879d617bfc70d89efe3600acf791de34ea3e
trang.pham <fresher.trang.pham@mohasoftware.com>
2022-10-10 11:28:40 +0700

    feat: 🚗 540/fixcs

--- src/app/Http/Controllers/Auth/RegisteredUserController.php
+++ src/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -29,18 +29,22 @@ class RegisteredUserController extends Controller
 
     public function store(Request $request)
     {
+
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
         event(new Registered($user));
         Auth::login($user);
+
         return redirect(RouteServiceProvider::HOME);
     }
 }
