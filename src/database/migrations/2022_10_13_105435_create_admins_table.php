e7057203161c3747ca9f9347335b3ff5dbb7b213
do.dinh <fresher.do.dinh@mohasoftware.com>
2022-10-13 15:41:12 +0700

    feat: 🚗 Admin Login

---
+++ src/database/migrations/2022_10_13_105435_create_admins_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name', 30)->nullable();
+            $table->string('account', 30)->unique();
+            $table->string('password');
+            $table->string('role', 30);
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+};
